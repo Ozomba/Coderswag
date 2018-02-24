55c6ef6a33aa9f9988f9c5fc39d2d7a36c8af576
Ozomba <ctakang@msn.com>
2018-02-24 15:54:43 -0500

    Custom adapter view

--- app/src/main/java/com/example/ctaka/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/ctaka/coderswag/Model/Category.kt
@@ -4,4 +4,8 @@ package com.example.ctaka.coderswag.Model
  * Created by ctaka on 2018-02-24.
  */
 class Category(val title: String, val image: String) {
+
+    override fun toString(): String {
+        return title
+    }
 }
